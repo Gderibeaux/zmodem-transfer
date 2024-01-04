69636552a167c6b04dffc94657e79a1d136948ca
Gus Deribeaux <Gusderibeaux@gmail.com>
2024-01-04 16:37:20 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
